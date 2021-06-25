c7e64456125b21ff15c34c3815358fa7121e8f4b
miaojing <a1024621284@163.com>
2021-06-25 16:01:29 +0800

    feat: request封装

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,5 @@
 <template>
   <h1>欢迎来到登录界面</h1>
-  <Welcome msg="jack" />
   <el-button @click="goHome">回首页</el-button>
 </template>
 
@@ -11,6 +10,23 @@ export default {
   components: {
     Welcome,
     // Welcome: () => import("./Welcome.vue"),
+  },
+  mounted() {
+    // this.$request({
+    //   method: "get",
+    //   url: "/login",
+    //   data: {
+    //     name: "Jack",
+    //     lastName: "Flintstone",
+    //   },
+    // }).then((res) => {
+    //   console.log(res);
+    // });
+    this.$request
+      .get("/login", { name: "jack" }, { mock: true, loading: true })
+      .then((res) => {
+        console.log(res);
+      });
   },
   methods: {
     goHome() {
--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css';
-import axios from 'axios'
-import config from './config'
+import request from './utils/request'
 
 // console.log("环境变量===>",process.env);
 console.log("环境变量===>", import.meta.env);
 const app = createApp(App)
+app.config.globalProperties.$request = request
 // app.mount('#app')
-axios.get(config.mockApi + "/login").then((res) => {
-  console.log(res);
-})
+// axios.get(config.mockApi + "/login").then((res) => {
+//   console.log(res);
+// })
 app.use(router).use(ElementPlus).mount("#app")
